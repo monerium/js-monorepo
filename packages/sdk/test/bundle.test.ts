62e0077f6672014c8c720b1b4b4f6d6fcc529502
einaralex <einaralex@gmail.com>
2024-07-26 16:24:10 +0000

    chore: bypass tests in CI

--- packages/sdk/test/bundle.test.ts
+++ packages/sdk/test/bundle.test.ts
@@ -17,15 +17,16 @@ const {
 test('should import without throwing', () => {
   expect(MoneriumClient).toBeDefined();
 });
-test('CommonJs bundle smoke test', async () => {
-  const client = new MoneriumClient({
-    clientId: APP_ONE_CREDENTIALS_CLIENT_ID,
-    clientSecret: APP_ONE_CREDENTIALS_SECRET,
+process.env.CI !== 'true' &&
+  test('CommonJs bundle smoke test', async () => {
+    const client = new MoneriumClient({
+      clientId: APP_ONE_CREDENTIALS_CLIENT_ID,
+      clientSecret: APP_ONE_CREDENTIALS_SECRET,
+    });
+
+    await client.getAccess();
+
+    const authContext = await client.getAuthContext();
+
+    expect(authContext.userId).toBe(APP_ONE_OWNER_USER_ID);
   });
-
-  await client.getAccess();
-
-  const authContext = await client.getAuthContext();
-
-  expect(authContext.userId).toBe(APP_ONE_OWNER_USER_ID);
-});
