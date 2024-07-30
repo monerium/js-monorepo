85c58240ebb4b0aee04e2e0f3ce37f7e52c29c1c
einaralex <einaralex@gmail.com>
2024-07-30 14:27:52 +0000

    chore: cleanup

--- apps/customer/components/App/Providers/Providers.tsx
+++ apps/customer/components/App/Providers/Providers.tsx
@@ -15,19 +15,12 @@ const queryClient = new QueryClient();
 
 const baseUrl = typeof window !== 'undefined' ? window.location.origin : '';
 
-console.log(
-  '%c baseUrl',
-  'color:white; padding: 30px; background-color: darkgreen',
-  baseUrl
-);
-
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>
           <MoneriumProvider
-            // clientId="f2cd22fa-2406-11ef-8cfc-fe34ee86fd51"
             clientId="f99e629b-6dca-11ee-8aa6-5273f65ed05b"
             redirectUrl={`${baseUrl}/dashboard`}
             environment="sandbox"
