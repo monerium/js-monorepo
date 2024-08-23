e177fca2a6d2be4e7f09eccd86f8e461445ddca5
einaralex <einaralex@gmail.com>
2024-08-23 01:45:25 +0000

    docs: add baseUrl for project

--- apps/developer/docusaurus.config.ts
+++ apps/developer/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://monerium.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/js-monorepo/',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'monerium', // Usually your GitHub org/user name.
