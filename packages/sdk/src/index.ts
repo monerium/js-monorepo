8546f2f9474a9ad89c64af2a856299e7f3d743f2
einaralex <einaralex@gmail.com>
2024-11-25 14:07:41 +0000

    feat: add short iban to order message

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -30,7 +30,13 @@
 import { MoneriumClient } from './client';
 export { default as constants } from './constants';
 export * from './types';
-export { placeOrderMessage, rfc3339, getChain, parseChain } from './utils';
+export {
+  placeOrderMessage,
+  rfc3339,
+  getChain,
+  parseChain,
+  shortenIban,
+} from './utils';
 
 export { MoneriumClient };
 export default MoneriumClient;
