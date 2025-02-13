39a54c3602b9f9e1f1cc00b168869b2d17f96335
einaralex <einaralex@gmail.com>
2025-02-13 07:23:18 +0000

    fix: simpler

--- apps/customer/hooks/useEns.ts
+++ apps/customer/hooks/useEns.ts
@@ -5,19 +5,8 @@ import { publicClient } from 'src/components/App/client';
 export const useEns = (address: string) => {
   return useQuery({
     queryKey: ['ens', address],
-    queryFn: async () => {
-      if (!address) return null;
-
-      try {
-        const ensName = await publicClient.getEnsName({
-          address: address as `0x${string}`,
-        });
-        return ensName;
-      } catch (error) {
-        console.error('Error fetching ENS name:', error);
-        return null;
-      }
-    },
+    queryFn: () =>
+      publicClient.getEnsName({ address: address as `0x${string}` }),
     enabled: !!address,
   });
 };
