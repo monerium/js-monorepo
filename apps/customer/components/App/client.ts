a211515c777075c71364facfaa1e0fcf660592b4
einaralex <einaralex@gmail.com>
2026-08-19 01:32:25 +0000

    fix: deps

--- apps/customer/components/App/client.ts
+++ apps/customer/components/App/client.ts
@@ -1,7 +1,7 @@
-import { createPublicClient, http } from 'viem';
+import { createPublicClient, http, type PublicClient } from 'viem';
 import { mainnet } from 'viem/chains';
 
-export const publicClient = createPublicClient({
+export const publicClient: PublicClient = createPublicClient({
   chain: mainnet,
   transport: http(),
 });
